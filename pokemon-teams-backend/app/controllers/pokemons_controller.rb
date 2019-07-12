b3fefd723054aec54a1e887e8d2d2302a87e621f
maxsunnyday <max.sun@yale.edu>
2019-07-12 15:13:18 -0400

    done

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,21 @@
 class PokemonsController < ApplicationController
+    def create
+        name = Faker::Name.first_name
+        species = Faker::Games::Pokemon.name
+        pokemon = Pokemon.create(species: species, nickname: name, trainer_id: params[:trainer_id])
+        render json: pokemon
+    end
+    
+    def destroy
+        pokemon = Pokemon.find(params[:id])
+        pokemon.destroy
+    end
+
+    private
+    
+    # def pokemon_params
+    #     name = Faker::Name.first_name
+    #     species = Faker::Games::Pokemon.name
+    #     params.permit(:species, :nickname, :trainer_id)
+    # end
 end
